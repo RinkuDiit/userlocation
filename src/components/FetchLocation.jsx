c6ede482c7dc245b7597e0d3d707cb5305c1a67c
Rinku <rr1341570@gmail.com>
2024-07-27 11:21:49 +0530

    table change

--- src/components/FetchLocation.jsx
+++ src/components/FetchLocation.jsx
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from 'react'
 function FetchLocation() {
 
     const [pincode, setPincode] = useState(0)
-    const [postofficeneme, setPostoffice] = useState({})
-    const [location, setLocation] = useState([])
+    const [postofficeneme, setPostoffice] = useState('')
+    const [postofficeses, setpostofficeses] = useState(0)
+    const [location, setLocation] = useState(null)
+
+
+    console.log('post'+{postofficeses});
     console.log(location);
 
 
@@ -14,6 +18,9 @@ function FetchLocation() {
         const response = await fetch(`https://api.postalpincode.in/pincode/${pincode}`)
         const data = await response.json()
         setLocation(data[0].PostOffice);
+
+      
+
     }
     fetchLocations();
     },[pincode])
@@ -21,10 +28,7 @@ function FetchLocation() {
 
     
     
-    const postoffice = (e) =>{
-       setPostoffice(e.target.value )
-       
-    }
+   
     const pincode_ = (e) =>{
         setPincode(e.target.value)
     }
@@ -32,14 +36,25 @@ function FetchLocation() {
         e.preventDefault();
 
     }
+    const handle_post = (e) => {
+        debugger
+        setPostoffice(e.target.value);
+        const postofficeNumer = location.findIndex((postoffice) => postoffice.Name === e.target.value);
+        setpostofficeses(postofficeNumer);
+        // console.log(location.findIndex((postoffice) => postoffice.Name === e.target.value));
+       
+      };
+    
+   
+
+
 
     if(!location){
         return(
             <>
               <div className="fillform">
             <form action="" onSubmit={submitedata} >
-                    <input type="text" onChange={postoffice} name="loacation" placeholder="Enter postoffice name" />  <br /> <br />
-                    <input type="text" onChange={pincode_} name='pincode' placeholder='Enter a Pin code' /> <br />
+             <input type="text" onChange={pincode_} name='pincode' placeholder='Enter a Pin code' /> <br />
             </form>
        </div>   
             </>
@@ -47,38 +62,97 @@ function FetchLocation() {
         )
 
     }
+
+//  console.log(location[{postofficeses}].Name);
     
 
   return (
     <div>
        <div className="fillform">
             <form action="" onSubmit={submitedata} >
-                    <input type="text" onChange={postoffice} name="loacation" placeholder="Enter postoffice name" />  <br /> <br />
-                    <input type="text" onChange={pincode_} name='pincode' placeholder='Enter a Pin code' /> <br />
+                    <input type="text" onChange={pincode_} name='pincode' placeholder='Enter a Pin code' /> <br /> <br />
+                     <select onChange={handle_post}  name="" id="">
+                    {location.map((postoffice) =>(
+                        <option  name={location.indexOf(postoffice.Name, location)}>{postoffice.Name}</option>
+
+                    ))}
+                    </select>
             </form>
        </div>
 
-       <div className="locationdata">
-        <h1>Location Data</h1>
-            <table>
+       <div className="locationdata" >
+        <p className='h3'>Location Data</p>
+        <p className=''>Postoffice: {postofficeneme}</p>
+        {/* <h2>length:{postofficeses}</h2> */}
+            {/* <table>
                 <tr>
                     <th>Postoffice Name</th>
                     <th>Pincode</th>
+                    <th>Block</th>
+                    <th>District</th>
                     <th>State</th>
                     <th>Country</th>
+
                 </tr>
-                    {location.map((postoffice) =>(
+                {postofficeses >= 0 ? (
                  <tr>
-                            <td>{postoffice.Name}</td>
-                            <td>{postoffice.Pincode}</td>
-                            <td>{postoffice.State}</td>
-                            <td>{postoffice.Country}</td>
-                            </tr>
-                    ))}
+                    
+                            <td>{location[postofficeses].Name}</td>
+                            <td>{location[postofficeses].Pincode}</td>
+                            <td>{location[postofficeses].Block}</td>
+                            <td>{location[postofficeses].District}</td>
+                            <td>{location[postofficeses].State}</td>
+                            <td>{location[postofficeses].Country}</td>
+                </tr>
+                    ) : (
+                        <tr>
+                          <td colSpan="4">No location data available</td>
+                        </tr>
+                      )}
 
                  
-            </table>
-       </div>
+            </table> */}
+            <table class="table">
+  
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <th>Post Office Name</th>
+      <td>{location[postofficeses].Name}</td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <th>Pincode</th>
+        <td>{location[postofficeses].Pincode}</td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <th>Block</th>
+        <td>{location[postofficeses].Block}</td>
+    </tr>
+    <tr>
+        <th scope="row">4</th>
+        <th>District</th>
+        <td>{location[postofficeses].District}</td>
+
+    </tr>
+    <tr>
+        <th scope="row">5</th>
+        <th>State</th>
+        <td>{location[postofficeses].State}</td>
+    </tr>
+    <tr>
+        <th scope="row">6</th>
+        <th>Country</th>
+        <td>{location[postofficeses].Country}</td>
+    </tr>
+
+   
+   
+  </tbody>
+</table>
+
+       </div>  
     </div>
   )
 }
